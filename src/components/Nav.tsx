d68128326c3932a989e2da65e043ebfc6766071b
Dave Inoc <michael828inoc@gmail.com>
2021-03-29 21:32:54 +0800

    Added Number of Items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../store";
+import { loadMovies } from "../store/movies/moviesAction";
+import { MovieList } from "../store/movies/moviesType";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+
+  const movies = useSelector<AppState, MovieList["movies"]>(
+    (state) => state.moviesReducer.movies
+  );
+
+  useEffect(() => {
+    dispatch(loadMovies());
+  }, [dispatch]);
+
   return (
     <header>
       <h3>DAVE INOC</h3>
-      <p>LIST OF ITEMS: </p>
+      <p>LIST OF ITEMS: {movies.length}</p>
     </header>
   );
 };
